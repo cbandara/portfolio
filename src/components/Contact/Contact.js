bc8e5155f40295a5db3be6797a2b8cd4441fddce
Charutha Bandara <charuthabandara@gmail.com>
2020-02-29 01:33:17 -0500

    blank target security patch

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,9 +8,9 @@ class Contact extends React.Component {
         <section className="section">
           <div className="container">
             <h1 className="work-heading">Shoot me an email!</h1>
-            <a href="mailto:mail@charuthabandara@gmail.com" target="_blank"><div className="work-button">Contact Me</div></a>
-            <a href="https://www.linkedin.com/in/cbandara/" target="_blank"><div className="work-button">LinkedIn</div></a>
-            <a href="https://github.com/cbandara" target="_blank"><div className="work-button">Github</div></a>
+            <a href="mailto:mail@charuthabandara@gmail.com" target="_blank" rel="noopener noreferrer"><div className="work-button">Contact Me</div></a>
+            <a href="https://www.linkedin.com/in/cbandara/" target="_blank" rel="noopener noreferrer"><div className="work-button">LinkedIn</div></a>
+            <a href="https://github.com/cbandara" target="_blank" rel="noopener noreferrer"><div className="work-button">Github</div></a>
           </div>
         </section>
       </div>
